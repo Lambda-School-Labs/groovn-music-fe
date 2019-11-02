df625edaf9ed2052157f96702dd8a71c82678684
unknown <dj.zaragoza@icloud.com>
2019-11-02 11:20:22 -0700

    creating client to start fetching data in index.js file

--- groovin-music/src/index.js
+++ groovin-music/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost'; //or you can use `import gql from 'graphql-tag';
+
+const client = new ApolloClient({ //client ready to fetch data
+  uri: '',
+});
+
+client  
+  .query({
+    query: gql`
+      {
+        rates(currency)
+      }
+  })
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
