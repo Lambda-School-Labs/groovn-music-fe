a6466fa60ab7f8fe64af66bc1090ed06dec54cae
unknown <dj.zaragoza@icloud.com>
2019-11-02 12:16:30 -0700

    finished building ApolloProvider and useQuery component to fetch testing data

--- groovin-music/src/index.js
+++ groovin-music/src/index.js
@@ -6,16 +6,33 @@ import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
 import { gql } from 'apollo-boost'; //or you can use `import gql from 'graphql-tag';
 import { render } from 'react-dom';
+import { useQuery } from '@apollo/react-hooks';
 
 import { ApolloProvider } from '@apollo/react-hooks';
 
-const App = () => {
-    <ApolloProvider client={client}>
-        <div>
-            <h2>Connecting FE to BE!</h2>
+const EXCHANGE_RATES = gql`
+    {
+        rates(currency: "USD") {
+            currency
+            rate
+        }
+    }
+`;
+
+function ExchangeRates() {
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.rates.map(({ currency, rate}) => (
+        <div key={currency}>
+            <p>
+                {currency}: {rate}
+            </p>
         </div>
-    </ApolloProvider>
-};
+    ));
+}
 
 const client = new ApolloClient({ //client ready to fetch data
   uri: 'http://localhost:4000/graphql',
@@ -33,7 +50,15 @@ client
   })
   .then(result => console.log(result));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+  const App = () => {
+    <ApolloProvider client={client}>
+        <div>
+            <h2>Connecting FE to BE!</h2>
+        </div>
+    </ApolloProvider>
+};
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
