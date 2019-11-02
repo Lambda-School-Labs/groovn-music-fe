9adf5652a4fd2d6ef4df064f83957884d621dbda
unknown <dj.zaragoza@icloud.com>
2019-11-02 11:53:16 -0700

    downloaded MongoDB Community Server and  imported ApolloProvider for local host

--- groovin-music/src/index.js
+++ groovin-music/src/index.js
@@ -5,18 +5,33 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
 import { gql } from 'apollo-boost'; //or you can use `import gql from 'graphql-tag';
+import { render } from 'react-dom';
+
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const App = () => {
+    <ApolloProvider client={client}>
+        <div>
+            <h2>Connecting FE to BE!</h2>
+        </div>
+    </ApolloProvider>
+};
 
 const client = new ApolloClient({ //client ready to fetch data
-  uri: '',
+  uri: 'http://localhost:4000/graphql',
 });
 
 client  
   .query({
     query: gql`
       {
-        rates(currency)
+        rates(currency: "USD") {
+            currency
+        }
       }
+      `
   })
+  .then(result => console.log(result));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
