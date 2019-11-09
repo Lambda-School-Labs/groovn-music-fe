a2ec72cc544b0826fce57a18a6411b355efaaf0b
Pherpher089 <Christutor089@gmail.com>
2019-11-09 10:47:37 -0800

    Changed render to component for the rout in App.js

--- groovin-music/src/App.js
+++ groovin-music/src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Route } from "react-router-dom";
-import "./App.css";
-import LandingPage from "./components/landingPage/landingPage.js";
+import React from 'react';
+import { Route } from 'react-router-dom';
+import './App.css';
+import LandingPage from './components/landingPage/landingPage.js';
 
 function App() {
-	return (
-		<div className="App">
-			<Route path="/" render={LandingPage}></Route>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Route path="/" component={LandingPage}></Route>
+    </div>
+  );
 }
 
 export default App;
--- groovin-music/src/components/landingPage/landingPage.js
+++ groovin-music/src/components/landingPage/landingPage.js
@@ -1,23 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 const OnChange = e => {};
 
 function LandingPage() {
-	// const [usernameInput, setUsernameInput] = useState("");
-	// const [passwordInput, setPasswordInput] = useState("");
+  const [usernameInput, setUsernameInput] = useState('');
+  const [passwordInput, setPasswordInput] = useState('');
 
-	return (
-		<>
-			<div className="Header">
-				<h1>Hello from Groovn Music Landing Page!</h1>
-			</div>
-			<div className="Body">
-				<form>
-					<input type="text" placeholder="username"></input>
-					<input type="password" placeHolder="password"></input>
-				</form>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="Header">
+        <h1>Hello from Groovn Music Landing Page!</h1>
+      </div>
+      <div className="Body">
+        <form>
+          <input type="text" placeholder="username"></input>
+          <input type="password" placeHolder="password"></input>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
