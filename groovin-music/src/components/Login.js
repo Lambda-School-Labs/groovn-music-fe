57f1655fe7d4712ec985da3bcc756c2b1562d354
Alan Lemasney <alanlemasney34@yahoo.com>
2019-11-14 23:36:18 -0700

    sign up component created along with custom hook for sign up

---
+++ groovin-music/src/components/Login.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function login() {
+    return (
+        <div>
+            <input></input>
+            <input></input>
+        </div>
+    )
+}
---
+++ groovin-music/src/components/Signup.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { useSignUpForm } from '../customHooks/CustomHooks.js';
+
+export default function Signup() {
+
+    const { inputs, handleInputChange, handleSubmit } = useSignUpForm();
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>First Name</label>
+                        <input type="text" name="firstName" onChange={handleInputChange} value={inputs.firstName} required />
+                    <label>Last Name</label>
+                        <input type="text" name="lastName" onChange={handleInputChange} value={inputs.lastName} required />
+                </div>
+
+                <div>
+                    <label>Email Address</label>
+                        <input type="email" name="email" onChange={handleInputChange} value={inputs.email} required />
+                </div>
+
+                <div>
+                    <label>Password</label>
+                        <input type="password" name="password" onChange={handleInputChange} value={inputs.password} required />
+                </div>
+
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+    )
+}
