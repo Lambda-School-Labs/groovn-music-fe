43adc6aaa1e6cea7c30b0fca75d8d7bb1357537d
Pherpher089 <Christutor089@gmail.com>
2019-11-14 18:46:47 -0800

    work on landing page

--- groovin-music/src/components/landingPage/landingPage.js
+++ groovin-music/src/components/landingPage/landingPage.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
-const OnChange = e => {};
+import React, { useReducer } from 'react';
 
 function LandingPage() {
-  const [usernameInput, setUsernameInput] = useState('');
-  const [passwordInput, setPasswordInput] = useState('');
+  const [userInput, setUserInput] = useReducer(
+    (state, newState) => ({ ...state, ...newState }),
+    {
+      username: '',
+      password: '',
+    }
+  );
 
+  const handleInput = ({ target: { name, value } }) => {
+    setUserInput({ [name]: value });
+  };
   return (
     <>
       <div className="Header">
@@ -12,8 +19,19 @@ function LandingPage() {
       </div>
       <div className="Body">
         <form>
-          <input type="text" placeholder="username"></input>
-          <input type="password" placeHolder="password"></input>
+          <input
+            type="text"
+            placeholder="username"
+            value={userInput.username}
+            onChange={handleInput}
+          />
+          <input
+            type="password"
+            placeHolder="password"
+            value={userInput.password}
+            onChange={handleInput}
+          ></input>
+          <button>Login</button>
         </form>
       </div>
     </>
