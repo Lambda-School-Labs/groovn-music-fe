13b9daad280768e8a4ef21cfba9bbce28f3bd1a7
unknown <dj.zaragoza@icloud.com>
2019-11-16 18:02:35 -0800

    finished debugging and pushing navbar.js file

--- groovin-music/src/App.js
+++ groovin-music/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from '../components/navbar.js';
 import { Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/landingPage/landingPage.js';
