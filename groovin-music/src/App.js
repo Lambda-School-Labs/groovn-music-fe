9db4061def8ebc2a58d83d56d9495cbd0b10602a
unknown <jjamal95@gmail.com>
2019-12-02 20:25:38 -0600

    updated context's state

--- groovin-music/src/App.js
+++ groovin-music/src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import TrackList from './Tracklist'; 
+import {MusicPlayerProvider} from "./MusicPlayerContext"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <MusicPlayerProvider>
+    <div className= "container">
+      <TrackList/>
     </div>
+   </MusicPlayerProvider>
   );
 }
+
 
 export default App;
