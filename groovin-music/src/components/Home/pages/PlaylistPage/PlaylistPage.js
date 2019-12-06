6209f8bfe8c8cf249175a9952f2e0c62458cb208
Pherpher089 <Christutor089@gmail.com>
2019-12-05 18:38:27 -0800

    Small changes

--- groovin-music/src/components/Home/pages/PlaylistPage/PlaylistPage.js
+++ groovin-music/src/components/Home/pages/PlaylistPage/PlaylistPage.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import Sidebar from '../../Sidebar/Sidebar.js';
+import playlistCover from '../../images/genaric_playlist_img.jpg';
+import './PlaylistPage.css';
+
 function PlaylistPage() {
   return (
     <div className="playlistPage-container">
       <div className="playlistPage-main">
         <h1>Playlist Main Content</h1>
+        <div className="playlist-name-container">
+          <h2>playlist name</h2>
+        </div>
+        <div className="playlist-image-container">
+          <Track />
+        </div>
       </div>
     </div>
   );
 }
 
 export default PlaylistPage;
+
+function Track(props) {
+  return (
+    <>
+      <img
+        className="playlist-image-container"
+        src={require('../../images/genaric_playlist_img.jpg')}
+        atl="Genaric_playlist_img"
+      />
+    </>
+  );
+}
