bcd05805ae9d6462126f882b7f451340f07f70b9
Pherpher089 <Christutor089@gmail.com>
2019-12-14 10:08:30 -0800

    Added private rout component for redirect

--- groovin-music/src/App.js
+++ groovin-music/src/App.js
@@ -1,18 +1,47 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
 import Homepage from './components/Home/Homepage.js';
 import LandingPage from './components/landingPage/landingPage';
-
 import './App.css';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
+import PrivateRoute from './components/PrivateRoute/PrivateRoute.js';
+
+const USER_QUERY = gql`
+  query currentUser {
+    currentUser {
+      displayName
+    }
+  }
+`;
+
+const User = () => {
+  console.log('here');
+  const { loading, error, data } = useQuery(USER_QUERY, {
+    notifyOnNetworkStatusChange: true,
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  } else if (error) {
+    return <div>{error.message}</div>;
+  }
+  console.log(data);
+  const { displayName } = data.currentUser;
+
+  return <div>{displayName}</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Homepage} />
-      <Route path="/landing-page" component={LandingPage} />
+      <Route to exact path="/landing-page" component={LandingPage} />
+      <Route exact to="/protected" component={PrivateRoute} />
     </div>
   );
 }
 
 export default App;
+
+//      <Route path="/" component={Homepage} />
--- groovin-music/src/components/Home/pages/HomeMain/HomeMain.js
+++ groovin-music/src/components/Home/pages/HomeMain/HomeMain.js
@@ -5,37 +5,38 @@ import MixTapes from './MixTapes.js';
 import FeaturedArtist from './FeaturedArtist.js';
 import Playlists from './Playlists.js';
 
-// import { gql } from 'apollo-boost';
-// import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-// const USER_QUERY = gql`
-//   query currentUser {
-//     currentUser {
-//       displayName
-//     }
-//   }
-// `;
+const USER_QUERY = gql`
+  query currentUser {
+    currentUser {
+      displayName
+    }
+  }
+`;
 
-// const User = () => {
-//   console.log('here');
-//   const { loading, error, data } = useQuery(USER_QUERY, {
-//     notifyOnNetworkStatusChange: true,
-//   });
+const User = () => {
+  console.log('here');
+  const { loading, error, data } = useQuery(USER_QUERY, {
+    notifyOnNetworkStatusChange: true,
+  });
 
-//   if (loading) {
-//     return <div>Loading...</div>;
-//   } else if (error) {
-//     return <div>{error.message}</div>;
-//   }
-//   console.log(data);
-//   const { displayName } = data.currentUser;
+  if (loading) {
+    return <div>Loading...</div>;
+  } else if (error) {
+    return <div>{error.message}</div>;
+  }
+  console.log(data);
+  const { displayName } = data.currentUser;
 
-//   return <div>{displayName}</div>;
-// };
+  return <div>{displayName}</div>;
+};
 
 function HomeMain() {
   return (
     <div className="homeMain-main">
+      <User />
       <AlbumOfWeek />
       <RecentlyPlayed />
       <MixTapes />
