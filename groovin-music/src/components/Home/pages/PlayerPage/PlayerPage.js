5fcba033fd715a3594be52ab7109fa94a6a85e44
Pherpher089 <Christutor089@gmail.com>
2019-12-22 20:41:30 -0800

    Added mutation in player component to create a recomended playlist and the play it. Added buttons for skipping tracks

--- groovin-music/src/components/Home/pages/PlayerPage/PlayerPage.js
+++ groovin-music/src/components/Home/pages/PlayerPage/PlayerPage.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './PlayerPage.css';
-import PlayerControls from '../../../MusicPlayer/PlayerControls.js';
+
 import SpotifyPlayer from 'react-spotify-player';
+import { gql } from 'apollo-boost';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+
+const CREATE_RECOMMENDED_PLAYLIST = gql`
+  mutation CreateRecommendedPlaylist($name: String!, $description: String) {
+    createRecommendedPlaylist(name: $name, description: $description) {
+      tracks
+    }
+  }
+`;
+
+export default function PlayerPage() {
+  return (
+    <div className="playerPage-container">
+      <RecommendedPlayist />
+    </div>
+  );
+}
 
 const size = {
   width: '100%',
@@ -10,32 +28,98 @@ const size = {
 const view = 'list'; // or 'coverart'
 const theme = 'black'; // or 'white'
 
-export default function PlayerPage() {
-  return (
-    <div className="playerPage-container">
-      <CurrentSong track={currentSong} />
-    </div>
-  );
-}
-
-function CurrentSong(props) {
-  return (
-    <div className="current-song">
-      <Player />
-    </div>
-  );
-}
-
-function Player() {
+function Player(props) {
   return (
     <SpotifyPlayer
-      uri="spotify:track:2IcjuFPUZISzX2LLhKZJN0"
+      uri={`spotify:track:${props.currentTrack}`}
       size={size}
       view={view}
       theme={theme}
     />
   );
 }
+
+const RecommendedPlayist = () => {
+  console.log('here in the reced playlist function');
+  const [input, setInput] = useState({
+    name: '',
+    description: '',
+  });
+
+  const [tracks, setTracks] = useState([]);
+  const [currentTrack, setCurrentTrack] = useState(0);
+
+  const [recommendedPlayist, { loading, data }] = useMutation(
+    CREATE_RECOMMENDED_PLAYLIST
+  );
+
+  const handleNameInput = event => {
+    setInput({ name: event.target.value, description: input.description });
+  };
+
+  const handleDescInput = event => {
+    setInput({ name: input.name, description: event.target.value });
+  };
+
+  const createPlaylist = async e => {
+    e.preventDefault();
+
+    const playlist = await recommendedPlayist({
+      variables: { name: input.name, description: input.description },
+    });
+
+    setInput({
+      name: '',
+      description: '',
+    });
+    setTracks(playlist.data.createRecommendedPlaylist.tracks);
+  };
+
+  const nextTrack = () => {
+    if (currentTrack < tracks.length - 1) {
+      setCurrentTrack(currentTrack + 1);
+    } else {
+      setCurrentTrack(0);
+    }
+  };
+
+  const previousTrack = () => {
+    if (currentTrack > 0) {
+      setCurrentTrack(currentTrack - 1);
+    } else {
+      setCurrentTrack(tracks.length - 1);
+    }
+  };
+
+  if (loading) {
+    return <div>{loading}</div>;
+  } else {
+    return (
+      <div>
+        <form onSubmit={createPlaylist}>
+          <input
+            type="text"
+            placeholder="Playlist Name"
+            name="name"
+            value={input.name}
+            onChange={handleNameInput}
+          />
+          <input
+            type="text"
+            placeholder="Playlist Description"
+            name="description"
+            value={input.description}
+            onChange={handleDescInput}
+          />
+          <button type="submit"> Create Playlist </button>
+        </form>
+        <Player currentTrack={tracks[currentTrack]} />
+        <button onClick={previousTrack}>Previous Track</button>
+        <button onClick={nextTrack}>Next Track</button>
+      </div>
+    );
+  }
+};
 
 const currentSong = {
   songTitle: 'Test-SongTitle',
